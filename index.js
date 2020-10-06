22c55b2140966402add5c35734528c3eae7bf8da
Anthony Mugendi <ngurumugz@gmail.com>
2020-10-06 04:52:24 +0300

    added renderInline method

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const marked = require('marked')
 exports.name = 'marked'
 exports.outputFormat = 'html'
 exports.inputFormats = ['marked', 'markdown', 'md']
-exports.render = function (str, options) {
-  return marked(str, options)
+exports.render = function(str, options) {
+    return marked(str, options)
+}
+
+exports.renderInline = function(str, options) {
+    return marked.parseInline(str, options)
 }
